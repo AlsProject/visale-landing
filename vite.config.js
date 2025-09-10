0d393e9334709d6568d17ed0f5e3eec09047e7ac
djaffar <djaffar@LAP05762.ALS.lan>
2025-09-10 11:17:30 +0200

    Fix vite config for development

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/visale-landing/'
+  base: process.env.NODE_ENV === 'production' ? '/visale-landing/' : '/'
 })
